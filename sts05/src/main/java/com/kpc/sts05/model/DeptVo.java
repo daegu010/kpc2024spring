7cf4df6e2da28a95687c28a643fac9b0eacc2fe1
busanbit <busanbit@gmail.com>
2024-05-31 16:45:28 +0900

    day05

--- sts05/src/main/java/com/kpc/sts05/model/DeptVo.java
+++ sts05/src/main/java/com/kpc/sts05/model/DeptVo.java
@@ -1,15 +1,24 @@
 package com.kpc.sts05.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+@Entity
+@Setter
 public class DeptVo {
+	@Id
+	@GeneratedValue
 	int deptno;
 	String dname,loc;
 }
