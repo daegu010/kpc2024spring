770c8325970e672af9a77c750377636dad79183e
busanbit <busanbit@gmail.com>
2024-05-31 16:32:25 +0900

    day05

--- sts05/src/main/java/com/kpc/sts05/HomeController.java
+++ sts05/src/main/java/com/kpc/sts05/HomeController.java
@@ -1,39 +1,59 @@
 package com.kpc.sts05;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-/**
- * Handles requests for the application home page.
- */
+import com.kpc.sts05.model.DeptVo;
+import com.kpc.sts05.service.DeptService;
+
 @Controller
 public class HomeController {
+	@Autowired
+	DeptService service;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
+	public String home() {
 		return "home";
 	}
 	
+	@GetMapping("/dept/")
+	public @ResponseBody List<DeptVo> list() {
+		return service.getList();
+	}
+	
+	@PostMapping("/dept/")
+	public ResponseEntity<?> add(DeptVo bean) {
+		if(bean==null) return ResponseEntity.badRequest().build();
+		service.addDb(bean);
+		return ResponseEntity.ok().build();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- sts05/src/main/webapp/WEB-INF/views/home.jsp
+++ sts05/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,89 @@
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ page session="false" pageEncoding="utf-8"%>
 <html>
 <head>
 	<title>Home</title>
+	<meta charset="utf-8">
+	<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+<div id="menu">
+	<a href="#p1">home</a>
+	<a href="#p2">dept</a>
+	<a href="#p3"></a>
+	<a href="#p4"></a>
+</div>
+<div id="p1">
+	<h2>index page</h2>
+</div>
+<div id="p2">
+	<h2>list page</h2>
+	<button>입력</button>
+	<table>
+		<thead>
+			<tr>
+				<th>부서번호</th>
+				<th>부서명</th>
+				<th>지역</th>
+			</tr>
+		</thead>
+		<tbody></tbody>
+	</table>
+</div>
+<div id="p3"></div>
+<div id="p4">
+	<h2>입력 페이지</h2>
+	<div>dname<input/></div>
+	<div>loc<input/></div>
+	<button>입력</button>
+</div>
+<script type="text/javascript">
+$('#p2,#p3,#p4').hide();
 
-<P>  The time on the server is ${serverTime}. </P>
+$('#p2>button').click(e=>{
+	$('#p4').find('input').eq(0).val("");
+	$('#p4').find('input').eq(1).val("");
+	$('#p4').show();
+});
+$('#p4>button').click(e=>{
+	var dname=$(e.target).parent().find('input').eq(0).val();
+	var loc=$(e.target).parent().find('input').eq(1).val();
+	$.post('dept/',{dname,loc},e=>$('#menu>a').eq(1).click());
+});
+
+$('#menu>a').eq(0).click(e=>{
+	$('#p2,#p3,#p4').hide();
+	$('#p1').show();
+});
+$('#menu>a').eq(1).click(e=>{
+	$('#p1,#p3,#p4').hide();
+	$('#p2').show();
+	$.get('dept/',data=>{
+		$('table>tbody').empty();
+		$(data).each((idx,ele)=>{
+			var tr=$('<tr/>');
+			$("<td>"+ele.deptno+"</td>").appendTo(tr);
+			$("<td>"+ele.dname+"</td>").appendTo(tr);
+			$("<td>"+ele.loc+"</td>").appendTo(tr);
+			$('table>tbody').append(tr);
+		});
+		
+	});
+});
+</script>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
